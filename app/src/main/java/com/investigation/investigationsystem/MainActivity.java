ae4e96f3bd53f35f8de7fc3d83e2ba190db0c20b
zhangbin <359529353@qq.com>
2016-06-27 21:48:59 +0800

    git记录账户密码测试1

--- app/src/main/java/com/investigation/investigationsystem/MainActivity.java
+++ app/src/main/java/com/investigation/investigationsystem/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 来自张彬的测试
+        // 来自张彬的测试
 
 
     }
