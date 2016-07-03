4427e0f060700febe3195e0ac11cda448a28c42c
Zero <847434035@qq.com>
2016-07-03 16:50:20 +0800

    意见建议页面 控件获取

---
+++ app/src/main/java/com/investigation/investigationsystem/business/suggest/presenter/SuggestPresenter.java
@@ -0,0 +1,63 @@
+package com.investigation.investigationsystem.business.suggest.presenter;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.investigation.investigationsystem.business.suggest.view.SuggestActivity;
+import com.investigation.investigationsystem.business.suggest.view.SuggestFragment;
+import com.investigation.investigationsystem.business.update.view.UpdateFragment;
+import com.investigation.investigationsystem.common.base.BaseFragmentActivity;
+import com.investigation.investigationsystem.common.base.BasePresenter;
+import com.investigation.investigationsystem.common.constants.StringConstants;
+
+/**
+ * Created by zero on 2016/7/3.
+ */
+public class SuggestPresenter extends BasePresenter{
+
+    private static final String TAG = StringConstants.TAG + SuggestPresenter.class.getName();
+    private static SuggestPresenter persenter;
+
+    private SuggestPresenter(BaseFragmentActivity rootActivity) {
+        this.rootActivity = rootActivity;
+    }
+
+    public static void regist(BaseFragmentActivity rootActivity) {
+        persenter = new SuggestPresenter(rootActivity);
+    }
+
+    public static SuggestPresenter getInstance() {
+        return persenter;
+    }
+
+    public static void startUp(Activity activity) {
+        Intent intent = new Intent(activity, SuggestActivity.class);
+        activity.startActivity(intent);
+        intent = null;
+    }
+
+    /**
+     * 加载默认的 fragment 页面
+     */
+    public void addDefaultFragment() {
+        replaceFragment(SuggestFragment.newInstance(), rootActivity.getFragmentViewID());
+    }
+
+    /**
+     * 提交意见建议
+     */
+    public void commitFeedBack(){
+
+    }
+
+
+    /**
+     * 销毁，回收资源
+     */
+    public void onDes() {
+        persenter = null;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/investigation/investigationsystem/business/suggest/view/SuggestFragment.java
@@ -0,0 +1,73 @@
+package com.investigation.investigationsystem.business.suggest.view;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.investigation.investigationsystem.R;
+import com.investigation.investigationsystem.business.update.presenter.UpdatePresenter;
+import com.investigation.investigationsystem.common.base.BaseTitleFragemnt;
+import com.investigation.investigationsystem.common.constants.StringConstants;
+
+/**
+ * Created by zero on 2016/7/3.
+ */
+public class SuggestFragment extends BaseTitleFragemnt{
+
+
+    private EditText et_commit; //意见建议输入框
+    private Button btn_commit; // 提交按钮
+
+    public static SuggestFragment newInstance() {
+        return new SuggestFragment();
+    }
+
+    @Override
+    protected int getContentViewID() {
+        return R.layout.fragment_suggest;
+    }
+
+    @Override
+    protected void initHeadView() {
+        setTitleStyle(true, false, StringConstants.TITLE_SUGGEST);
+    }
+
+    @Override
+    protected void InitHeadClick() {
+        head_view_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpdatePresenter.getInstance().goBack();
+            }
+        });
+    }
+
+    @Override
+    protected void analyzeBundle(Bundle bundle) {
+        return ;
+    }
+
+    @Override
+    protected void onCreateByUser() {
+
+    }
+
+    @Override
+    protected void initView() {
+        et_commit = (EditText) rootView.findViewById(R.id.suggest_et_commit);
+        btn_commit = (Button) rootView.findViewById(R.id.suggest_btn_commit);
+    }
+
+    @Override
+    protected void initClick() {
+        //提交按钮点击事件
+        btn_commit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+}
